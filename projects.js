1627c5717f4d89550ffe55e3de5c072a51c9368e
Azeemuddin Aziz <79792351+work-azeem08@users.noreply.github.com>
2022-06-13 11:24:31 +0530

    Initial commit

---
+++ projects.js
@@ -0,0 +1,28 @@
+const projectArray = [
+  {
+    name: "Product Listing Site",
+    desc: "The page has form which takes necessary inputs to describe a product and then the submitted info gets displayed to card shown in website.",
+    image: "./media/project-images/Project-1.png",
+    websiteLink: "#",
+    sourceCodeLink: "#",
+    tags: ["HTML", "CSS", "JS"],
+  },
+  {
+    name: "Responsive Navbar",
+    desc: "This page consist of a responsive navbar, that is on the smaller device it will look more neat and cleaner.",
+    image: "./media/project-images/Project-2.png",
+    websiteLink: "#",
+    sourceCodeLink: "#",
+    tags: ["HTML", "CSS", "JS"],
+  },
+  {
+    name: "Card Flipper",
+    desc: "Here is a flipable card where on one side there is a few details like name and social media handles. Whereas on the other side it has description about the person and his skills.",
+    image: "./media/project-images/Project-3.png",
+    websiteLink: "#",
+    sourceCodeLink: "#",
+    tags: ["HTML", "CSS"],
+  },
+];
+
+export default projectArray;
---
+++ script.js
@@ -0,0 +1,58 @@
+// Script for side menu toggling.
+const menu = document.querySelector(".nav-links");
+const toggleBtn = document.querySelector(".ham-menu");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  toggleBtn.classList.toggle("bx-menu-alt-right");
+});
+
+document.addEventListener("click", function (evt) {
+  if (!menu.classList.contains("active")) return;
+  var isNav = menu.contains(evt.target) || toggleBtn.contains(evt.target);
+  if (!isNav) {
+    menu.classList.remove("active");
+    toggleBtn.classList.remove("bx-menu-alt-right");
+  }
+});
+
+// Script for project pane's project display
+import projectArray from "./projects.js";
+
+const projectPane = document.querySelector(".project-pane");
+
+projectArray.forEach((project) => {
+  const card = document.createElement("div");
+  card.classList.add("big-card");
+
+  const tags = document.createElement("div");
+  tags.classList.add("tags");
+  const tagsArray = project.tags;
+  tagsArray.forEach((tag) => {
+    const span = document.createElement("span");
+    span.innerHTML = tag;
+    tags.append(span);
+  });
+
+  const content = `
+    <div class="big-card__left">
+          <img src="${project.image}" alt="project-image" />
+          <div class="action-btns">
+              <a target="_blank" href="${project.websiteLink}">Website</a>
+              <a target="_blank" href="${project.sourceCodeLink}"><i class="bx bx-download"></i></a>
+          </div>
+          </div>
+      <div class="big-card__right">
+          <div class="title"><i>${project.name}</i></div>
+          <div class="description">
+              ${project.desc}
+          </div>
+          <div class="tags">
+              ${tags.innerHTML}
+          </div>
+      </div>
+      `;
+
+  card.innerHTML = content;
+  projectPane.append(card);
+});
